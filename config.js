3b61762514e1194405910a12fb22aaf2247d15b8
lymbow442 <lhuillier.evan2008@gmail.com>
2026-03-09 23:38:43 +0100

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://supabase.com/dashboard/project/slqvnuerfaomyudjyjwt'
-
-const SUPABASE_URL = 'https://slqvnuerfaomyudjyjwt.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_rmWBKI31bvjTNgefIkkr4A_JvM8LgZ5'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
