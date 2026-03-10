eee5b6991ff89cb9607e9752580eb98ffdb613ec
lymbow442 <lhuillier.evan2008@gmail.com>
2026-03-10 13:36:27 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://slqvnuerfaomyudjyjwt.supabase.co";
+const SUPABASE_KEY = "sb_publishable_rmWBKI31bvjTNgefIkkr4A_JvM8LgZ5";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
